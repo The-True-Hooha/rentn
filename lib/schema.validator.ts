b700d92ca3f93a18b966d6fe473cb737046234b3
David Ogar <owogogahhero@outlook.com>
2023-03-25 16:04:56 +0100

    validate schema for every req body

--- lib/schema.validator.ts
+++ lib/schema.validator.ts
@@ -26,13 +26,6 @@ export function agentSchemaValidation(data: {}){
         password: zod
             .string()
             .min(8, { message: 'Password must be at least 8 characters long' })
-            .regex(
-              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-              {
-                message:
-                  'Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol',
-              },
-            )
             .max(16, 'password is too long')
             .nonempty('password must be set by the user')
             .trim(),
