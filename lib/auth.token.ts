6e79d078c1eed781ad03c38578b15e0e3264f0fd
David Ogar <owogogahhero@outlook.com>
2023-04-11 19:06:26 +0100

    refactor: create access token

--- lib/auth.token.ts
+++ lib/auth.token.ts
@@ -1,12 +1,14 @@
 import { Secret, sign } from 'jsonwebtoken'
 
+export const createAccessToken = (id: string, role: string): string => {
+  const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as Secret;
+  const expiresIn = process.env.ACCESS_TOKEN_REFRESH_TIME;
 
-export const createAccessToken = (id: string, role: string) => {
-    return sign(
-        { id: id, role: role },
-        process.env.ACCESS_TOKEN_SECRET as Secret,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_REFRESH_TIME,
-        }
-    )
+  const token = sign(
+    { id, role },
+    accessTokenSecret,
+    { expiresIn }
+  );
+
+  return token;
 }
