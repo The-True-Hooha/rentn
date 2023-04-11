ff9b39a3b936aa0b695c19b633bc9414026b59ed
David Ogar <owogogahhero@outlook.com>
2023-04-11 18:26:28 +0100

    feat: assign jwt to user login and as sessions

--- src/pages/api/v1/auth/agent/login.ts
+++ src/pages/api/v1/auth/agent/login.ts
@@ -1,8 +1,10 @@
 import bcrypt from "bcrypt"
 import { prisma } from "config/prisma.connect";
 import { AgentLoginDto } from "dto/agent.dto.interface";
+import { createAccessToken } from "lib/auth.token";
 import { agentLoginSchema } from "lib/schema.validator";
 import { NextApiRequest, NextApiResponse } from "next";
+import { serialize } from "cookie"
 
 export default async function handler (
     req: NextApiRequest, 
@@ -38,11 +40,23 @@ export default async function handler (
                 message: 'password or email is incorrect',
                 date: new Date(),
             });
+        } else {
+            const setCookie = serialize(
+                'rentn',
+                createAccessToken(agent.agentId, agent.role),{
+                    httpOnly: false,
+                    sameSite: "strict",
+                    maxAge: 60 * 60 * 24 * 3, //expires in 3 days
+                    path: '/',
+                }
+            )
+            res.setHeader('rentn-cookie', setCookie)
+            return res.status(200).send({
+                message: 'login successful',
+                date: new Date(),
+            })
         }
-        return res.status(200).send({
-            message: 'login successful',
-            date: new Date(),
-        })
+        
     } catch(error: any) {
         console.log("Agent login error::", error);
         return res.status(500).send({
