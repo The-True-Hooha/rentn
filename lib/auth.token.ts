8addaa787ad17bc4a7bdf44ad20b11ef1667c3a5
David Ogar <owogogahhero@outlook.com>
2023-04-11 18:25:18 +0100

    feat: create access token

---
+++ lib/auth.token.ts
@@ -0,0 +1,12 @@
+import { Secret, sign } from 'jsonwebtoken'
+
+
+export const createAccessToken = (id: string, role: string) => {
+    return sign(
+        { id: id, role: role },
+        process.env.ACCESS_TOKEN_SECRET as Secret,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_REFRESH_TIME,
+        }
+    )
+}
