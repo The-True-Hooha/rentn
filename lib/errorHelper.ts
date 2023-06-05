80b26f994b4def7910d9a5e78d12b36cfbe84d40
Davidhero <owogogahhero@outlook.com>
2023-06-05 16:30:11 +0100

    refactor: minor changes

--- lib/errorHelper.ts
+++ lib/errorHelper.ts
@@ -18,3 +18,21 @@ export function errorHelper (
         date: new Date(),
     }
 }
+
+// TODO: refactor all api responses to include custom error helpers
+
+    // if (/* some condition */) {
+    //     const errorResponse = createErrorResponse(400, 'Bad Request');
+    //     return res.status(errorResponse.statusCode).json(errorResponse);
+    //   }
+  
+    //   If no error occurs, send a success response
+    //   return res.status(200).json({ message: 'Success' });
+    // } catch (error: any) {
+    //   Handle unexpected errors
+    //   console.error('An unexpected error occurred:', error);
+  
+    //   Create an error response with a generic message
+    //   const errorResponse = createErrorResponse(500, 'Internal Server Error');
+    //   return res.status(errorResponse.statusCode).json(errorResponse);
+    // }
