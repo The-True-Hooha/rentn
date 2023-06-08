502c3811f6df1cfee9abd83485e834ff9344ddac
Davidhero <owogogahhero@outlook.com>
2023-06-08 10:38:17 +0100

    refactor: named function

--- lib/hash.password.helper.ts
+++ lib/hash.password.helper.ts
@@ -1,6 +1,6 @@
 import { genSalt, hash } from 'bcrypt';
 
-export default async function hashPassword(password: string): Promise<string> {
+export default async function hashString(password: string): Promise<string> {
   const saltRounds = Number(process.env.SALT_ROUNDS);
   const salt = await genSalt(saltRounds);
   const hashedPassword = await hash(password, salt);
