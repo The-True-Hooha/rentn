717e5b7dd314e40a864fa27a936952d781feaa41
Davidhero <owogogahhero@outlook.com>
2023-06-23 17:28:57 +0100

    feat: add auth token with jwt

--- lib/auth.token.ts
+++ lib/auth.token.ts
@@ -1,14 +1,40 @@
-import { Secret, sign } from 'jsonwebtoken'
+import { Secret, sign, verify, JwtPayload, TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken'
 
-export const createAccessToken = (id: string, role: string): string => {
+export const createAccessToken = (id: string, email: string, role?: string): string => {
   const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as Secret;
   const expiresIn = process.env.ACCESS_TOKEN_REFRESH_TIME;
 
   const token = sign(
-    { id, role },
+    { id, email },
     accessTokenSecret,
     { expiresIn }
   );
 
   return token;
 }
+
+export interface AccessTokenPayload extends JwtPayload {
+  id: string;
+  email: string;
+  role?: string;
+}
+
+export function verifyAccessToken(token: string, secret: Secret): AccessTokenPayload {
+  try {
+    const payload = verify(token, secret) as AccessTokenPayload;
+    if (payload.id && payload.email) {
+      return payload;
+    }
+    throw new Error("Error! Invalid access token");
+  } catch (error: any) {
+    if (error instanceof TokenExpiredError) {
+      throw new Error("sorry, the access token has expired");
+    } else if (error instanceof JsonWebTokenError) {
+      throw new Error("Invalid access token");
+    } else {
+      throw error;
+    }
+  }
+}
+
+
